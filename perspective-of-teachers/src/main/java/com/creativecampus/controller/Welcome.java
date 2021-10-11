c3bce650c907064b8ef9092a576eb58968502e60
DinoStark <15190304050@163.com>
2021-10-12 00:37:58 +0800

    Generate entities and mappers.

---
+++ perspective-of-teachers/src/main/java/com/creativecampus/controller/Welcome.java
@@ -0,0 +1,20 @@
+package com.creativecampus.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/home")
+@Slf4j
+public class Welcome
+{
+    @RequestMapping("welcome")
+    @ResponseBody
+    public String welcome()
+    {
+        log.info("Enter welcome");
+        return "Welcome";
+    }
+}
