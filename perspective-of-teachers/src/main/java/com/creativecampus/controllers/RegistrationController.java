dd59ac9f10593c7217c95f06b597d80676c19074
DinoStark <15190304050@163.com>
2021-10-24 16:09:14 +0800

    Complete the development of log-and-validate annotation.

--- perspective-of-teachers/src/main/java/com/creativecampus/controllers/RegistrationController.java
+++ perspective-of-teachers/src/main/java/com/creativecampus/controllers/RegistrationController.java
@@ -2,7 +2,6 @@ package com.creativecampus.controllers;
 
 import com.creativecampus.commons.ServiceResponse;
 import com.creativecampus.commons.arguments.LogAndValidate;
-import com.creativecampus.commons.arguments.logs.LogArguments;
 import com.creativecampus.commons.domain.Teacher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
